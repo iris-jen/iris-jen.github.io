67f45ec885634b5bfe426996aa5fbdc3a6e776b3
Iris Jennison <iris.jennis@gmail.com>
2025-06-20 16:51:12 -0400

    remove dead album

--- src/app/data/Albums.js
+++ src/app/data/Albums.js
@@ -77,29 +77,13 @@ const albums = [
      details: 'First part of a comp of some old stuff. To lazy to post all the songs here + github pages have limited storage /r/n'+
      'so check out the links!',
     links: {
-      appleMusic: null,
-      spotify: "https://open.spotify.com/album/6BHgDeK86etfW3bPhysO6s",
-      itunes: "https://geo.music.apple.com/album/odds-and-ends-volume-1/1788977728",
-      youtubeMusic: "https://music.youtube.com/playlist?list=OLAK5uy_n3AaG95ccPtI0OZL6XzST2bkbmydXbH0c",
-      deezer: "https://www.deezer.com/album/693459451"
+      appleMusic: "https://music.apple.com/ca/album/1788977728?app=music&at=1l3vpUI&ct=LFV_ba64ba3fc59e0eb8da4990312dabed59&itscg=30440&itsct=catchall_p1&lId=211872662&cId=none&sr=1&src=Linkfire&ls=1",
+      spotify: "https://open.spotify.com/album/6BHgDeK86etfW3bPhysO6s?go=1",
+      itunes: "https://music.apple.com/ca/album/1788977728?app=itunes&at=1l3vpUI&ct=LFV_ba64ba3fc59e0eb8da4990312dabed59&itscg=30440&itsct=catchall_p3&lId=211872662&cId=none&sr=3&src=Linkfire&ls=1",
+      youtubeMusic: "https://music.youtube.com/playlist?list=OLAK5uy_nUdb6pjPwsy9WjJOrjGyBRR3aO49Etkbc&src=Linkfire&lId=02c48f89-363d-4a12-b580-603b13935ef9&cId=d3d58fd7-4c47-11e6-9fd0-066c3e7a8751",
+      deezer: "https://www.deezer.com/album/693459451?app_id=140685&utm_source=partner_linkfire&utm_campaign=ba64ba3fc59e0eb8da4990312dabed59&utm_medium=Original&utm_term=prod&utm_content=album-693459451"
     }
   },
-  {
-    id: 'oo2',
-    name: 'Odds and ends. Volume 2',
-    recordedYear: 2013,
-    writtenYear: 2011,
-    cover: '/music/oo2/cover.webp',
-     details: 'To lazy to post all the songs here + github pages have limited storage /r/n'+
-     'so check out the links!',
-    links: {
-      appleMusic: "https://geo.music.apple.com/album/odds-and-ends-volume-2/1787590246",
-      spotify: "https://open.spotify.com/album/4K1jv7LczuuTmY23yTyAa2",
-      itunes: null,
-      youtubeMusic: "https://music.youtube.com/playlist?list=OLAK5uy_n3AaG95ccPtI0OZL6XzST2bkbmydXbH0c",
-      deezer: "https://open.qobuz.com/album/inyy73kad65cc"
-    }
-  }
 ];
 
 export default albums;
